4ba5a44ea432812fa308aa2af15b5421637da4b7
Will Berman <WLBberman@gmail.com>
2023-08-04 23:09:14 +0000

    more

--- arch_changes.py
+++ arch_changes.py
@@ -2,6 +2,7 @@ import torch
 from torch import nn
 from torch.utils.benchmark import Compare, Timer
 
+from muse import MaskGiTUViT
 from muse.transformer_profiling import LayerNormOptionalBias
 from muse.transformer_profiling import MaskGitUVit as MaskGitUVitProfile
 from muse.transformer_profiling import SingleProjTransformerFFN
@@ -42,12 +43,47 @@ class UnfusedRMSNorm(nn.Module):
 
 results = []
 
+batch_sizes = [1, 2, 4, 8, 16, 32]
+
+model = MaskGiTUViT.from_pretrained("../finetune_text_encoder/checkpoint-186000/unwrapped_model/")
+model.to("cuda", dtype=torch.float16)
+model.set_use_memory_efficient_attention_xformers(True)
+model.eval()
+
+for batch_size in batch_sizes:
+    input_ids = torch.randint(0, 1000, (batch_size, 576), device="cuda")
+    encoder_hidden_states = torch.randn((batch_size, 77, 768), device="cuda", dtype=torch.float16)
+
+    model(input_ids, encoder_hidden_states)
+
+    def benchmark_fn():
+        model(input_ids, encoder_hidden_states)
+
+    timer = Timer(
+        stmt="benchmark_fn()",
+        globals={"benchmark_fn": benchmark_fn},
+        num_threads=1,
+        description="base model (original code base)",
+        label=f"batch_size {batch_size}",
+    ).blocked_autorange(min_run_time=1)
+
+    results.append(timer)
+
+del model
+
 for config_name, model_args in [
     ("unfused_rms_norm", {"norm_cls": UnfusedRMSNorm}),
-    ("base model (no changes)", {}),
+    ("base model (refactored)", {}),
     ("replace rms norm with ln", {"norm_cls": LayerNormOptionalBias}),
     ("single proj ffn", {"ffn_cls": SingleProjTransformerFFN}),
     ("replace res with transformer", {"num_res_blocks": 0, "num_transformer_layers": 28}),
+    (
+        "replace rms norm with ln and single proj ffn",
+        {
+            "norm_cls": LayerNormOptionalBias,
+            "ffn_cls": SingleProjTransformerFFN,
+        },
+    ),
     (
         "all changes",
         {
@@ -60,10 +96,13 @@ for config_name, model_args in [
 ]:
     model = MaskGitUVitProfile(**model_args)
     model.to("cuda", dtype=torch.float16)
+    model.eval()
 
-    for batch_size in [1, 2, 4, 8, 16, 32]:
+    for batch_size in batch_sizes:
         input_ids = torch.randint(0, 1000, (batch_size, 576), device="cuda")
         encoder_hidden_states = torch.randn((batch_size, 77, 768), device="cuda", dtype=torch.float16)
+
+        model(input_ids, encoder_hidden_states)
 
         def benchmark_fn():
             model(input_ids, encoder_hidden_states)
